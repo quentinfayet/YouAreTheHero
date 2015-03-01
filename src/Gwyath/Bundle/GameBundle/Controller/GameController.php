d66c67fd08efcb3b8abfda8edb1d50d6b87aa3ad
quentinfayet <fayet.quentin@gmail.com>
2015-03-01 17:03:10 +0100

    + Refactored controller + Refactored template

--- src/Gwyath/Bundle/GameBundle/Controller/GameController.php
+++ src/Gwyath/Bundle/GameBundle/Controller/GameController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class GameController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/new-game")
      * @Template()
      */
-    public function indexAction($name)
+    public function newGameAction()
     {
-        return array('name' => $name);
+        return array();
     }
 }
