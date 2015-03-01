8f116cbe289116f9f9944766e4f03b8c6149c612
quentinfayet <fayet.quentin@gmail.com>
2015-03-01 17:01:21 +0100

    + Updated AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Gwyath\Bundle\LibraryBundle\GwyathLibraryBundle(),
+            new Gwyath\Bundle\GameBundle\GwyathGameBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
