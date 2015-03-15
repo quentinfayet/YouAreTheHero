f140e1f2d1d59d729fc51d5637afc698356f00b2
quentinfayet <fayet.quentin@gmail.com>
2015-03-15 12:35:11 +0100

    + Added BestiaryBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Gwyath\Bundle\GameBundle\GwyathGameBundle(),
             new Gwyath\Bundle\AdventureBundle\GwyathAdventureBundle(),
             new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
+            new Gwyath\Bundle\BestiaryBundle\GwyathBestiaryBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Gwyath/Bundle/BestiaryBundle/GwyathBestiaryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gwyath\Bundle\BestiaryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GwyathBestiaryBundle extends Bundle
+{
+}
